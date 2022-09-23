ff276c1cc780bb38e99d19988fb65ede78f728a0
AlyssaD <pink_lime@live.com>
2022-09-23 16:41:45 -0400

    implement index view

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,17 @@
 import { Flight } from '../models/flight.js'
 
 function index(req, res) {
-
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
 }
 
 export {
--- routes/flights.js
+++ routes/flights.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 const router = Router()
+import * as flightsCtrl from '../controllers/flights.js'
 
 /* GET users listing. */
 router.get('/', flightsCtrl.index)
