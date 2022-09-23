2a0b822d76d371d14424baae72bd9fcddd777fcf
AlyssaD <pink_lime@live.com>
2022-09-23 17:16:55 -0400

    create route for new

--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,8 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 /* GET users listing. */
 router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
 
 export {
   router
